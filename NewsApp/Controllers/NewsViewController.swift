d3787b1a8fbff0e9ecdf734cf5b6a7a05568e48b
Vladislav <lordroga2@gmail.com>
2021-08-04 09:53:45 +0300

    Initial Commit

---
+++ NewsApp/Controllers/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  NewsApp
+//
+//  Created by Владислав on 04.08.2021.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
